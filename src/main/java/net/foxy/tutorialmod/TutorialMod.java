cf8528a4819e1ff1611e0eb80ac2df6378456fa7
Ivo Guedes <1252042@isep.ipp.pt>
2025-11-30 22:05:13 +0000

    Added Mod blocks and items, need to fix raw pink garnet block texture

--- src/main/java/net/foxy/tutorialmod/TutorialMod.java
+++ src/main/java/net/foxy/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.foxy.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.foxy.tutorialmod.block.ModBlocks;
+import net.foxy.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +13,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+        ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
 	}
 }
